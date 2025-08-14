6e52e46228fb66907de08f721fc977147b1c8302
th3-buNNy-gUy <rounaqchoudhuri@gmail.com>
2025-08-14 21:11:09 +0530

    Code Push 1

---
+++ src/utility/plot.py
@@ -0,0 +1,173 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+warnings.filterwarnings("ignore")
+
+def plot_dimensional_reduction(df, title="Dimensional Reduction", dpi=100):
+    plt.figure(figsize=(20, 6), dpi=dpi)
+    scatter = sns.scatterplot(
+                        data=df, 
+                        x=df.columns[0],
+                        y=df.columns[1],
+                        hue="description", style="description", 
+                        edgecolor="black", zorder=5, alpha=0.7,
+                        palette="deep", s=100, legend="full"
+    )
+    sns.move_legend(
+                        scatter, "lower center",
+                        bbox_to_anchor=(.5, 1.1),
+                        ncol=5, title="Description", frameon=True,
+    )
+    plt.grid(True)
+    plt.title(title, fontsize=20)
+    plt.show()
+
+def make_confusion_matrix(cf,
+                          group_names=None,
+                          categories='auto',
+                          count=True,
+                          percent=True,
+                          cbar=True,
+                          xyticks=True,
+                          xyplotlabels=True,
+                          sum_stats=True,
+                          figsize=None,
+                          cmap='Blues',
+                          title=None):
+    '''
+    This function will make a pretty plot of an sklearn Confusion Matrix cm using a Seaborn heatmap visualization.
+
+    Arguments
+    ---------
+    cf:            confusion matrix to be passed in
+
+    group_names:   List of strings that represent the labels row by row to be shown in each square.
+
+    categories:    List of strings containing the categories to be displayed on the x,y axis. Default is 'auto'
+
+    count:         If True, show the raw number in the confusion matrix. Default is True.
+
+    normalize:     If True, show the proportions for each category. Default is True.
+
+    cbar:          If True, show the color bar. The cbar values are based off the values in the confusion matrix.
+                   Default is True.
+
+    xyticks:       If True, show x and y ticks. Default is True.
+
+    xyplotlabels:  If True, show 'True Label' and 'Predicted Label' on the figure. Default is True.
+
+    sum_stats:     If True, display summary statistics below the figure. Default is True.
+
+    figsize:       Tuple representing the figure size. Default will be the matplotlib rcParams value.
+
+    cmap:          Colormap of the values displayed from matplotlib.pyplot.cm. Default is 'Blues'
+                   See http://matplotlib.org/examples/color/colormaps_reference.html
+                   
+    title:         Title for the heatmap. Default is None.
+
+    '''
+
+
+    # CODE TO GENERATE TEXT INSIDE EACH SQUARE
+    blanks = ['' for i in range(cf.size)]
+
+    if group_names and len(group_names)==cf.size:
+        group_labels = ["{}\n".format(value) for value in group_names]
+    else:
+        group_labels = blanks
+
+    if count:
+        group_counts = ["{0:0.0f}\n".format(value) for value in cf.flatten()]
+    else:
+        group_counts = blanks
+
+    if percent:
+        div = np.array([np.sum(cf, axis=1) for _ in range(cf.shape[0])])
+        group_percentages = ["{0:.2%}".format(value/d) for value, d in zip(cf.flatten(), div.T.flatten())]
+    else:
+        group_percentages = blanks
+
+    box_labels = [f"{v1}{v2}{v3}".strip() for v1, v2, v3 in zip(group_labels,group_counts,group_percentages)]
+    box_labels = np.asarray(box_labels).reshape(cf.shape[0],cf.shape[1])
+
+
+    # CODE TO GENERATE SUMMARY STATISTICS & TEXT FOR SUMMARY STATS
+    if sum_stats:
+        #Accuracy is sum of diagonal divided by total observations
+        accuracy  = np.trace(cf) / float(np.sum(cf))
+
+        #if it is a binary confusion matrix, show some more stats
+        if len(cf)==2:
+            #Metrics for Binary Confusion Matrices
+            precision = cf[1,1] / sum(cf[:,1])
+            recall    = cf[1,1] / sum(cf[1,:])
+            f1_score  = 2*precision*recall / (precision + recall)
+            stats_text = "\n\nAccuracy={:0.3f}\nPrecision={:0.3f}\nRecall={:0.3f}\nF1 Score={:0.3f}".format(
+                accuracy,precision,recall,f1_score)
+        else:
+            stats_text = "\n\nAccuracy={:0.3f}".format(accuracy)
+    else:
+        stats_text = ""
+
+
+    # SET FIGURE PARAMETERS ACCORDING TO OTHER ARGUMENTS
+    if figsize==None:
+        #Get default figure size if not set
+        figsize = plt.rcParams.get('figure.figsize')
+
+    if xyticks==False:
+        #Do not show categories if xyticks is False
+        categories=False
+
+
+    # MAKE THE HEATMAP VISUALIZATION
+    plt.figure(figsize=figsize)
+    sns.heatmap(cf,
+                    annot=box_labels,
+                    fmt="",
+                    # cmap=cmap,
+                    cbar=cbar,
+                    xticklabels=categories,yticklabels=categories)
+
+    if xyplotlabels:
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label' + stats_text)
+    else:
+        plt.xlabel(stats_text)
+    
+    if title:
+        plt.title(title)
+
+def categorical_distribution(dataframe, column_name, title=None, save=False, save_path=None, save_format='png', save_dpi=100):
+    """
+    Plots the distribution of a categorical column with counts on top of bars.
+
+    Args:
+        dataframe (pd.DataFrame): The DataFrame containing the data.
+        column_name (str): The name of the categorical column to plot.
+        title (str, optional): The title of the plot. Defaults to None.
+    """
+    plt.figure(figsize=(15, 6), dpi=save_dpi) # Adjusted figure size for better readability
+    # if title: plt.suptitle(title, fontsize=20) # Used suptitle for overall title
+
+    # Create the countplot
+    ax = sns.countplot(y=column_name, data=dataframe, order=dataframe[column_name].value_counts().index, palette='viridis', stat="percent")
+
+    # Add count labels on top of each bar
+    for p in ax.patches:
+        width = p.get_width()    # Get the width of the bar (which is the count)
+        plt.text(width + 0.5,     # Set the x position of the text slightly to the right of the bar
+                 p.get_y() + p.get_height() / 2, # Set the y position at the middle of the bar
+                 '{:0.2f}%'.format(width), # Format the count to an integer
+                 ha='left', va='center', fontsize=10, color='black') # Text alignment and style
+
+    plt.xlabel('Percent', fontsize=12) # Label for x-axis
+    plt.ylabel(column_name.replace('_', ' ').title(), fontsize=12) # Formatted label for y-axis
+    plt.tight_layout(rect=[0, 0, 1, 0.96]) # Adjust layout to prevent title overlap
+
+    if save: plt.savefig(f"../../datasets/Skoltech_Anomaly_Benchmark/plots/{save_path}/{title}.{save_format}", format=save_format)
+    plt.show()
